6de9edef073d769106a12b353bee54fbe56b6f6d
unknown <ayodejiakinborewa@gmail.com>
2023-03-20 23:34:34 +0100

    feat: implemented navbar && footer components, updated layout

---
+++ src/assets/icons/bar.tsx
@@ -0,0 +1,13 @@
+const Bar = (
+  <svg
+    width="1"
+    height="16"
+    viewBox="0 0 1 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <line x1="0.5" y1="0.286026" x2="0.499999" y2="15.286" stroke="#FE8501" />
+  </svg>
+);
+
+export default Bar;
