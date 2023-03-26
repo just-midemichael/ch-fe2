2505aa0f4922e35aed98ced57cc4d50fbff7d831
Olajire Dominic <lerryjay45@gmail.com>
2023-03-26 11:42:26 +0100

    feat: added logic for dot scroll

--- src/components/shared/horizontalScroll/index.tsx
+++ src/components/shared/horizontalScroll/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useRef } from "react";
 import { H3, P } from "../headings/Headings";
 
 const HorizontalScroll: React.FC<{
@@ -7,12 +7,47 @@ const HorizontalScroll: React.FC<{
   subTitle?: string;
   children: React.ReactNode;
 }> = ({ subTitle, title, label, children }) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const scroll = (scrollOffset: number) => {
+    if (ref && ref.current) ref.current.scrollLeft += scrollOffset;
+  };
+
+  const scrollTo = (position: number) => {
+    ref.current?.scrollTo({
+      left: position,
+      behavior: "smooth"
+    });
+  };
+
+  const scrollWidth = useMemo(() => ref.current?.scrollWidth || 0, [ref]);
   return (
     <div className="flex flex-col">
-      <P>{label}</P>
-      <H3>{title}</H3>
-      <P>{subTitle}</P>
-      <div className="mt-10 flex overflow-x-scroll">{children}</div>
+      <div className="flex items-center justify-between">
+        <div>
+          <P>{label}</P>
+          <H3>{title}</H3>
+          <P>{subTitle}</P>
+        </div>
+        <div className="flex gap-3">
+          <button onClick={() => scroll(-100)}>prev</button>
+          <button onClick={() => scrollTo(0)}>dot1</button>
+          <button onClick={() => scrollTo(scrollWidth - (4 / 5) * scrollWidth)}>
+            dot2
+          </button>
+          <button onClick={() => scrollTo(scrollWidth - (3 / 5) * scrollWidth)}>
+            dot3
+          </button>
+          <button onClick={() => scrollTo(scrollWidth - (2 / 5) * scrollWidth)}>
+            dot4
+          </button>
+          <button onClick={() => scrollTo((5 / 5) * scrollWidth)}>dot5</button>
+          <button onClick={() => scroll(100)}>next</button>
+        </div>
+      </div>
+      <div className="mt-10 flex overflow-x-hidden" ref={ref}>
+        {children}
+      </div>
     </div>
   );
 };
