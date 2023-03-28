1930bd548ba08c83cc47c0967b98d8b31d01aeaa
Olajire Dominic <lerryjay45@gmail.com>
2023-03-28 18:54:40 +0100

    feat: completed carousel for hotel and room images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
-import Banner from "@/components/shared/banner/Banner";
+import Carousel from "@/components/shared/carousel/Carousel";
 import Meta from "@/components/shared/meta/Meta";
 
 export default function Home() {
+  const medias = ["/Banner.png", "/hotel.png", "/featured.png", "/city.png"];
+
   return (
     <>
       <Meta
         title="Welcome to City Hotels"
         description="Home page of City Hotels, Nigeria"
       />
-      <Banner />
+      <div className="mx-auto my-10 w-[683px]">
+        <Carousel medias={medias} />
+      </div>
     </>
   );
 }
