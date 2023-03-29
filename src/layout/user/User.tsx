1fc3bb55d120246fd6a69a02ac82b1c173f888c3
Olajire Dominic <lerryjay45@gmail.com>
2023-03-29 07:11:37 +0100

    feat: completed user dashbard layout

---
+++ src/layout/user/User.tsx
@@ -0,0 +1,19 @@
+import Sidenav from "@/components/user/sidenav/Sidenav";
+import type { PropsWithChildren } from "react";
+import React from "react";
+import Main from "../main/Main";
+
+const UserDashboardLayout: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <Main>
+      <div className="flex">
+        <div className="w-[315px]">
+          <Sidenav />
+        </div>
+        <div className="w-full">{children}</div>
+      </div>
+    </Main>
+  );
+};
+
+export default UserDashboardLayout;
