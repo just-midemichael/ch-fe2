34fedda90323c805593aae42747e65b4cebd6b6e
Olajire Dominic <olajiredominic@gmail.com>
2023-04-20 11:38:17 +0100

    feat: completed ui enhancement for hotel page

---
+++ src/components/hotel/hotelFacilityCard/HotelFacilityCard.props.ts
@@ -0,0 +1,7 @@
+export default interface HotelFacilityCardProps
+  extends React.HtmlHTMLAttributes<HTMLElement> {
+  className?: string;
+  title: string;
+  Icon: any;
+  description: string;
+}
--- src/components/shared/map/Map.tsx
+++ src/components/shared/map/Map.tsx
@@ -8,62 +8,55 @@ import { H4, H5, P } from "../headings/Headings";
 
 const Map: FC<MapProps> = () => {
   return (
-    <div className="absolute left-[690px] top-9 hidden md:block lg:left-[920px]">
+    <div className="absolute right-[130px] top-2 hidden  md:block">
       <div>
         <Img
           path="/map.png"
           name=""
-          className="h-[406px] w-[404px] rounded-lg"
+          className="h-[406px] w-[401px] rounded-lg"
         />
-        <div className="mt-4 flex items-center gap-3">
+        <div className="mt-5 flex items-center gap-3">
           <div className="rounded-full bg-[#FFF9F6] p-3">
             <GoLocation className="text-[#FE8501]" />
           </div>
           <P>67988 Felix Garden Apt. 387, York, Oregon, 69634</P>
         </div>
       </div>
-
-      <div className="my-7">
-        <H4 className="font-bold">What&apos;s Nearby?</H4>
-
-        <div className="flex items-center gap-3">
-          <div className="rounded-full bg-[#FFF9F6] p-3">
-            <FaWalking className="text-[#FE8501]" />
-          </div>
-
-          <div>
-            <H5>Walk Score</H5>
-            <P>57/100 (Somewhat Walkable)</P>
-          </div>
-        </div>
-      </div>
-
-      <div className="my-7">
-        <H4 className="font-bold">What&apos;s Nearby?</H4>
-
-        <div className="flex items-center gap-3">
-          <div className="rounded-full bg-[#FFF9F6] p-3">
-            <RiBus2Fill className="text-[#FE8501]" />
-          </div>
-
-          <div>
-            <H5>Transit Score</H5>
-            <P>40/100 (Somewhat Walkable)</P>
+      <H4 className="mt-7 font-semibold">What&apos;s Nearby?</H4>
+      <div className="mt-6 flex flex-col gap-6">
+        <div>
+          <div className="flex items-center gap-3">
+            <div className="rounded-full bg-[#FFF9F6] p-3">
+              <FaWalking className="text-[#FE8501]" />
+            </div>
+            <div>
+              <H5>Walk Score</H5>
+              <P>57/100 (Somewhat Walkable)</P>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="my-7">
-        <H4 className="font-bold">What&apos;s Nearby?</H4>
-
-        <div className="flex items-center gap-3">
-          <div className="rounded-full bg-[#FFF9F6] p-3">
-            <RiMotorbikeFill className="text-[#FE8501]" />
+        <div>
+          <div className="flex items-center gap-3">
+            <div className="rounded-full bg-[#FFF9F6] p-3">
+              <RiBus2Fill className="text-[#FE8501]" />
+            </div>
+            <div>
+              <H5>Transit Score</H5>
+              <P>40/100 (Somewhat Walkable)</P>
+            </div>
           </div>
+        </div>
 
-          <div>
-            <H5>Bike Score</H5>
-            <P>20/100 (Somewhat Walkable)</P>
+        <div>
+          <div className="flex items-center gap-3">
+            <div className="rounded-full bg-[#FFF9F6] p-3">
+              <RiMotorbikeFill className="text-[#FE8501]" />
+            </div>
+            <div>
+              <H5>Bike Score</H5>
+              <P>20/100 (Somewhat Walkable)</P>
+            </div>
           </div>
         </div>
       </div>
